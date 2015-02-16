975443a98b5e9667d726cdba945e1bc7b136338b
bahakz <bahakz@gmail.com>
2015-02-16 17:29:24 +0600

    Initial Commit

---
+++ Albumen iOS Client/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Albumen iOS Client
+//
+//  Created by Bakytzhan Baizhikenov on 2/16/15.
+//  Copyright (c) 2015 intellection. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
